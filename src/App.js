00779da17938705344fee48bc316626cb3684c4a
Nobisuke19 <subham019650@gmail.com>
2024-05-07 08:18:43 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Navbar from './Components/Navbar';
+import Home from './Components/Home';
+import Component from './Components/Component';
+import About from './Components/About';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/component" element={<Component />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Components/Home.js
@@ -0,0 +1,16 @@
+// src/components/Home.js
+import React from 'react';
+// import Table from './Table';
+import Data from './Data';
+
+const Home = () => {
+  return (
+    <div className='m-8'>
+      <h1>Home</h1>
+      {/* <Table /> */}
+      <Data/>
+    </div>
+  );
+}
+
+export default Home;
