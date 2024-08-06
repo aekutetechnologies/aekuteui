68885721071c784bec39284dccbcdd550c5158c5
Nobisuke19 <subham019650@gmail.com>
2024-08-07 00:59:15 +0530

    home page update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,38 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+// import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Components from "./components/Components";
 import About from "./components/Pricing";
 import Subscription from "./components/Subscription";
+// import Footer from "./components/Footer";
 
 import Alert from "./components/ui/Alert";
 import Button from "./components/ui/Buttons";
 import Card from "./components/ui/Card";
+import Test from "./components/ui/Test";
+
+import "./App.css";
 
 function App() {
   return (
     <Router>
-      <>
-        <div className="App">
-          <Navbar />
-          <div className="content">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/components/ui/alert" element={<Alert />} />
-              <Route path="/components/ui/button" element={<Button />} />
-              <Route path="/components/ui/card" element={<Card />} />
-              <Route path="/components" element={<Components />} />
-              <Route path="/pricing" element={<About />} />
-              <Route path="/pricing/subscription" element={<Subscription />} />
-            </Routes>
-          </div>
-          {/* <Footer/> */}
+      <div className="App ">
+        {/* <Navbar /> */}
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/components/ui/alert" element={<Alert />} />
+            <Route path="/components/ui/button" element={<Button />} />
+            <Route path="/components/ui/card" element={<Card />} />
+            <Route path="/components/ui/Test" element={<Test />} />
+            <Route path="/components" element={<Components />} />
+            <Route path="/pricing" element={<About />} />
+            <Route path="/pricing/subscription" element={<Subscription />} />
+            <Route path="/test" element={<Test/>}/>
+          </Routes>
         </div>
-      </>
+        {/* <Footer/> */}
+      </div>
     </Router>
   );
 }
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,37 +1,21 @@
 import { Link } from "react-router-dom";
-import { motion } from "framer-motion";
-
+import '../style.css'
 const Navbar = () => {
   return (
-    <motion.nav
-      className="fixed top-0 left-0 right-0 z-50 bg-transparent backdrop-blur backdrop-filter bg-opacity-30"
-      initial={{ backgroundColor: "transparent" }}
-      animate={{ backgroundColor: "#F9FAFB" }} // Light background color
-      transition={{ duration: 0.3 }}
-    >
-      <div className="container mx-auto flex items-center justify-between py-4">
-        <Link to="/" className="text-gray-900 text-4xl font-extrabold"> {/* Updated text color */}
-          AekuetUI
-        </Link>
-        <div className="flex items-center">
-          <input
-            type="text"
-            placeholder="Search..."
-            className="bg-gray-200 text-gray-800 rounded-md py-2 px-4 focus:outline-none" // Lighter input background and text color
-          />
-          <Link to="/components" className="text-gray-900 hover:text-gray-700 ml-4"> {/* Updated text color */}
-            Components
-          </Link>
-          <Link to="/pricing" className="text-gray-900 hover:text-gray-700 ml-4"> {/* Updated text color */}
-            Pricing
-          </Link>
-        </div>
+    <header className="bg-trasparent my-2">
+    <nav className="mx-auto container p-5 nav-card flex items-center justify-between flex-wrap">
+      <div className="">
+        <img src="" alt="logo" />
       </div>
-    </motion.nav>
+      <div className="flex gap-4">
+        <Link to="/elements">Components</Link>
+        <Link to="/pricing">Pricing </Link>
+        <Link to='/register'>Sign up</Link>
+      </div>
+    </nav>
+  </header>
   );
 };
 
 export default Navbar;
-
-
 
